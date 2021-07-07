d379de0a4abcd6eadb06317b812a1c0269420cfd
秋山諒次郎 <ryojiroakiyama@akiyamaryoujirounoMacBook-Air.local>
2021-07-07 17:39:29 +0900

    done get path

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include "./pipex.h"
 
 char **g_command;
+char *g_path;
+
+
+void end(void)__attribute__((destructor));
+
+void end(void)
+{
+    system("leaks pipex");
+}
+
+
+void free_2d_array(char **a)
+{
+	size_t i;
+
+	i = 0;
+	while (a[i] != NULL)
+	{
+		free(a[i]);
+		i++;
+	}
+	free(a);
+}
+
+void put_2d_array(char **a)
+{
+	if (a == NULL)
+	{
+		printf("array is NULL");
+		return ;
+	}
+	while (*a != NULL)
+	{
+		printf("%s\n", *a);
+		a++;
+	}
+}
 
 void ft_exit(char *s)
 {
@@ -8,34 +45,67 @@ void ft_exit(char *s)
 	exit(1);
 }
 
-void ft_perrexit(const char *s)
+void perrexit(const char *s)
 {
 	perror(s);
 	exit(1);
 }
 
-void extract(char *avi, char *path)
+char *get_path(char *command, char **envp)
 {
-	char 	**path_list;
-	int		flag;
+	size_t	i;
+	char	*tmp1;
+	char	*tmp2;
+	char	**path_list;
 
-	flag = 0;
-	g_command = ft_split(avi, ' ');
-	if (access(g_command, F_OK) == 0)
-		flag++;
+	i = 0;
+	path_list = ft_split((*envp) + 5, ':');
+	while (path_list[i])
+	{
+		tmp1 = ft_strjoin(path_list[i], "/");
+		tmp2 = ft_strjoin(tmp1, command);
+		free(tmp1);
+		if (access(tmp2, F_OK | X_OK) == 0)
+		{
+			free_2d_array(path_list);
+			return (tmp2);
+		}
+		free(tmp2);
+		i++;
+	}
+	free_2d_array(path_list);
+	return (NULL);
+}
+
+void set_command(char *av_command, char **envp)
+{
+	g_command = ft_split(av_command, ' ');
+	g_path = NULL;
+	if (access(g_command[0], F_OK | X_OK) == 0)
+		g_path = ft_strdup(g_command[0]);
 	else
 	{
-		path_list = ft_split(path, ';');
+		while (*envp && ft_strncmp(*envp, "PATH=", 5))
+			envp++;
+		if (*envp != NULL)
+			g_path = get_path(g_command[0], envp);
+	}
+	if (g_path == NULL)
+	{
+		free_2d_array(g_command);
+		ft_exit("no such file or permission denied");
+	}
+}
 		
 int	main(int ac, char **av, char **envp)
 {
-	size_t i;
-
-	if (ac != 5)
-		ft_exit("invalid number of arguments");
-	i = 0;
-	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5))
-		i++;
-	extract(av[1], &envp[i][5]);//challege with ls as av[1]
+	(void)ac;
+//	if (ac != 5)
+//		ft_exit("invalid number of arguments");
+	set_command(av[1], envp);//challege with ls as av[1]
+	put_2d_array(g_command);
+	printf("%s\n", g_path);
+	free_2d_array(g_command);
+	free(g_path);
 	return (0);
 }
